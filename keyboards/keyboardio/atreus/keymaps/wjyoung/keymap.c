4990d23de2219408d9f50999a7f36d3683db6047
Wayne Young <wjyoung65@gmail.com>
2022-11-13 17:38:00 -0500

    added mouse layer

--- keyboards/keyboardio/atreus/keymaps/wjyoung/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/wjyoung/keymap.c
@@ -23,6 +23,7 @@
 #define FUN 4
 #define NAV 5
 #define MED 6
+#define MOU 7  // mouse
 
 // Some handy macros to keep the keymaps clean and easier to maintain
 
@@ -59,7 +60,7 @@
 // right-side thumb keys: hold for a layer shift, tap for normal key
 #define LT_TAB  LT(NAV, KC_TAB)
 #define LT_SPC  KC_SPC
-#define LT_ESC  KC_ESC
+#define LT_ESC  LT(MOU, KC_ESC)
 
 #else  // my thumb keys
 
@@ -69,6 +70,7 @@
 #define LT_TAB  LT(SYM, KC_TAB)
 
 #define LT_MINS LT(NAV, KC_MINS)
+#define LT_QUOT LT(MOU, KC_QUOT)
 
 #endif
 
@@ -96,51 +98,57 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CMK] = LAYOUT(
 #ifdef USE_MIRYOKU
-    KC_Q,    KC_W,     KC_F,     KC_P,    KC_B,                       KC_J,    KC_L,    KC_U,     KC_Y,    KC_QUOT,
-    MT_A,    MT_R,     MT_S,     MT_T,    KC_G,                       KC_M,    MT_N,    MT_E,     MT_I,    MT_O,
-    KC_Z,    KC_X,     KC_C,     KC_D,    KC_V,    KC_GRV,  KC_BSLS,  KC_K,    KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,
-    BT_ESC,  KC_LALT,  KC_LGUI,  LT_DEL,  LT_BSPC, LT_ENT,  LT_TAB,   LT_SPC,  LT_ESC,  DB2,      KC_EXLM, BT_ENT
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
+    MT_A,    MT_R,    MT_S,    MT_T,    KC_G,                      KC_M,    MT_N,    MT_E,    MT_I,    MT_O,
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_GRV,  KC_BSLS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+    BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_BSPC, LT_ENT,  LT_TAB,  LT_SPC,  LT_ESC,  DB2,     KC_EXLM, BT_ENT
 #else
-    KC_Q,    KC_W,     KC_F,     KC_P,    KC_B,                       KC_J,    KC_L,    KC_U,     KC_Y,    KC_BSPC,
-    MT_A,    MT_R,     MT_S,     MT_T,    KC_G,                       KC_M,    MT_N,    MT_E,     MT_I,    MT_O,
-    KC_Z,    KC_X,     KC_C,     KC_D,    KC_V,    KC_GRV,  KC_BSLS,  KC_K,    KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,
-    BT_ESC,  KC_LALT,  KC_LGUI,  LT_DEL,  LT_SPC,  LT_TAB,  LT_MINS,  KC_SPC,  KC_QUOT, DB2,      KC_EXLM, BT_ENT
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_BSPC,
+    MT_A,    MT_R,    MT_S,    MT_T,    KC_G,                      KC_M,    MT_N,    MT_E,    MT_I,    MT_O,
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_GRV,  KC_BSLS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+    BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_SPC,  LT_TAB,  LT_MINS, KC_SPC,  LT_QUOT, DB2,     KC_EXLM, BT_ENT
 #endif
   ),
   [QWE] = LAYOUT(
-    KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,     KC_O,    KC_BSPC,
-    QT_A,    QT_S,     QT_D,     QT_F,    KC_G,                       KC_H,    QT_J,    QT_K,     QT_L,    QT_P,
-    KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,    KC_GRV,  KC_BSLS,  KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,
-    BT_ESC,  KC_LALT,  KC_LGUI,  LT_DEL,  LT_SPC,  LT_TAB,  LT_MINS,  KC_SPC,  KC_QUOT, DB2,      KC_EXLM, BT_ENT
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_BSPC,
+    QT_A,    QT_S,    QT_D,    QT_F,    KC_G,                      KC_H,    QT_J,    QT_K,    QT_L,    QT_P,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+    BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_SPC,  LT_TAB,  LT_MINS, KC_SPC,  KC_QUOT, DB2,     KC_EXLM, BT_ENT
   ),
   [NUM] = LAYOUT(
-    _______, _______,  _______,  _______, _______,                    KC_LBRC, KC_7,    KC_8,     KC_9,    KC_RBRC,
-    _______, _______,  _______,  _______, _______,                    KC_EQL,  KC_4,    KC_5,     KC_6,    KC_SCLN,
-    _______, _______,  _______,  _______, _______, _______, _______,  KC_BSLS, KC_1,    KC_2,     KC_3,    KC_GRV,
+    _______, _______, _______, _______, _______,                   KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,
+    _______, _______, _______, _______, _______,                   KC_EQL,  KC_4,    KC_5,    KC_6,    KC_SCLN,
+    _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_1,    KC_2,    KC_3,    KC_GRV,
 #ifdef USE_MIRYOKU
-    _______, _______,  _______,  _______, _______, _______, KC_MINS,  KC_0,    KC_DOT,  _______,  _______, _______
+    _______, _______, _______, _______, _______, _______, KC_MINS, KC_0,    KC_DOT,  _______, _______, _______
 #else
-    DF(CMK), KC_BTN2,  KC_BTN3,  KC_BTN1, _______, _______, KC_UNDS,  KC_0,    KC_DOT,  _______,  _______, _______
+    DF(CMK), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_UNDS, KC_0,    KC_DOT,  _______, _______, _______
 #endif
   ),
   [SYM] = LAYOUT(
-    _______, _______,  _______,  _______, _______,                    KC_LCBR, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RCBR,
-    _______, _______,  _______,  _______, _______,                    KC_PLUS, KC_DLR,  KC_PERC,  KC_CIRC, KC_COLN,
-    _______, _______,  _______,  _______, _______, _______, _______,  KC_PIPE, KC_EXLM, KC_AT,    KC_HASH, KC_TILD,
-    DF(CMK), _______,  _______,  _______, _______, _______, KC_UNDS,  KC_LPRN, KC_RPRN, _______,  _______, _______
+    _______, _______, _______, _______, _______,                   KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,
+    _______, _______, _______, _______, _______,                   KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_COLN,
+    _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD,
+    DF(CMK), _______, _______, _______, _______, _______, KC_EQL,  KC_LPRN, KC_RPRN, _______, _______, _______
   ),
   [FUN] = LAYOUT(
-    _______, _______,  _______,  _______, _______,                    KC_PSCR, KC_F7,   KC_F8,    KC_F9,   KC_F12,
-    _______, _______,  _______,  _______, _______,                    KC_SCRL, KC_F4,   KC_F5,    KC_F6,   KC_F11,
-    _______, _______,  _______,  _______, _______, _______, _______,  KC_PAUS, KC_F1,   KC_F2,    KC_F3,   KC_F10,
-    DF(CMK), _______,  _______,  _______, _______, _______, XXXXXXX,  XXXXXXX, KC_APP,  _______,  _______, _______
+    _______, _______, _______, _______, _______,                   KC_PSCR, KC_F7,   KC_F8,   KC_F9,   KC_F12,
+    _______, _______, _______, _______, _______,                   KC_SCRL, KC_F4,   KC_F5,   KC_F6,   KC_F11,
+    _______, _______, _______, _______, _______, _______, _______, KC_PAUS, KC_F1,   KC_F2,   KC_F3,   KC_F10,
+    DF(CMK), _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, KC_APP,  _______, _______, _______
   ),
   [NAV] = LAYOUT(
-    KC_PGUP, KC_HOME,  KC_UP,    KC_END,  KC_INS,                     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
-    KC_PGDN, KC_LEFT,  KC_DOWN,  KC_RGHT, CAPSWRD,                    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,
-    _______, _______,  _______,  _______, KC_CAPS, _______, _______,  XXXXXXX, KC_N,    XXXXXXX,  XXXXXXX, XXXXXXX,
-    DF(QWE), DF(CMK),  DF(NUM),  _______, _______, _______, _______,  _______, _______, _______,  _______, _______
+    KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, CAPSWRD,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    _______, _______, _______, _______, KC_CAPS, _______, _______, XXXXXXX, KC_N,    XXXXXXX, XXXXXXX, XXXXXXX,
+    DF(QWE), DF(CMK), DF(NUM), DF(MOU), _______, _______, _______, _______, _______, _______, _______, _______
     /* set default layer qwerty or colemak */
+  ),
+  [MOU] = LAYOUT( // mouse
+    KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    DF(CMK), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, _______, _______
   ),
 };
 
