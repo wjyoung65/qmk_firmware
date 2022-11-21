0b1934d3a3e1977706fda705f92494282ceaf7b0
Wayne Young <wjyoung65@gmail.com>
2022-11-20 21:34:46 -0500

    switch to qerty as base layer

--- keyboards/keyboardio/atreus/keymaps/wjyoung/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/wjyoung/keymap.c
@@ -3,10 +3,9 @@
 // to all 44 Atreus keys.
 
 // to be exact, this is the Miryoku
-//   * colemak-dh
+//   * qwerty colemak-dh
 //   * flip
 //   * inverted T
-//   * no mouse layer (never needed it)
 //   * holding down SPACE does not shift layers b/c sometimes I want easy auto repeat
 //   * backspace, enter, and escape in the corners
 //   * thumb keys adjusted, still experimenting
@@ -16,8 +15,8 @@
 //#define USE_MIRYOKU  // otherwise my map
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
-#define CMK 0  // default base: colemak
-#define QWE 1  // alternate base: qwerty
+#define QWE 0  // default base layer: qwerty
+#define CMK 1  // alternate: colemak
 #define NUM 2
 #define SYM 3
 #define FUN 4
@@ -95,6 +94,12 @@ enum custom_keycodes {
 ////
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [QWE] = LAYOUT(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_BSPC,
+    QT_A,    QT_S,    QT_D,    QT_F,    KC_G,                      KC_H,    QT_J,    QT_K,    QT_L,    QT_P,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+    BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_QUOT, LT_TAB,  LT_MINS, KC_SPC,  LT_SCLN, DB2,     KC_EXLM, BT_ENT
+  ),
   [CMK] = LAYOUT(
 #ifdef USE_MIRYOKU
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
@@ -108,12 +113,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_QUOT, LT_TAB,  LT_MINS, KC_SPC,  LT_SCLN, DB2,     KC_EXLM, BT_ENT
 #endif
   ),
-  [QWE] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_BSPC,
-    QT_A,    QT_S,    QT_D,    QT_F,    KC_G,                      KC_H,    QT_J,    QT_K,    QT_L,    QT_P,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    BT_ESC,  KC_LALT, KC_LGUI, LT_DEL,  LT_QUOT, LT_TAB,  LT_MINS, KC_SPC,  LT_SCLN, DB2,     KC_EXLM, BT_ENT
-  ),
   [NUM] = LAYOUT(
     _______, _______, _______, _______, _______,                   KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,
     _______, _______, _______, _______, _______,                   KC_EQL,  KC_4,    KC_5,    KC_6,    KC_SCLN,
@@ -121,20 +120,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef USE_MIRYOKU
     _______, _______, _______, _______, _______, _______, KC_MINS, KC_0,    KC_DOT,  _______, _______, _______
 #else
-    DF(CMK), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_UNDS, KC_0,    KC_DOT,  _______, _______, _______
+    DF(QWE), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_UNDS, KC_0,    KC_DOT,  _______, _______, _______
 #endif
   ),
   [SYM] = LAYOUT(
     _______, _______, _______, _______, _______,                   KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,
     _______, _______, _______, _______, _______,                   KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_COLN,
     _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD,
-    DF(CMK), _______, _______, _______, _______, _______, KC_EQL,  KC_LPRN, KC_RPRN, _______, _______, _______
+    DF(QWE), _______, _______, _______, _______, _______, KC_EQL,  KC_LPRN, KC_RPRN, _______, _______, _______
   ),
   [FUN] = LAYOUT(
     _______, _______, _______, _______, _______,                   KC_PSCR, KC_F7,   KC_F8,   KC_F9,   KC_F12,
     _______, _______, _______, _______, _______,                   KC_SCRL, KC_F4,   KC_F5,   KC_F6,   KC_F11,
     _______, _______, _______, _______, _______, _______, _______, KC_PAUS, KC_F1,   KC_F2,   KC_F3,   KC_F10,
-    DF(CMK), _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, KC_APP,  _______, _______, _______
+    DF(QWE), _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, KC_APP,  _______, _______, _______
   ),
   [NAV] = LAYOUT(
     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -147,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    DF(CMK), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, _______, _______
+    DF(QWE), _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, _______, _______
   ),
 };
 
